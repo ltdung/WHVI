472c0f39fc7ede36bd0454b841b1c15bf2b92067
David Nabergoj <davidnabergoj4@gmail.com>
2021-01-16 15:12:43 +0100

    C++ gradcheck works, but is not in place. In-place transformation mess up autograd.

--- src/fwht/cpp/fwht.cpp
+++ src/fwht/cpp/fwht.cpp
@@ -1,7 +1,8 @@
 #include <torch/extension.h>
 #include <vector>
 
-std::vector<torch::Tensor> fwht_forward(torch::Tensor x) {
+at::Tensor fwht(torch::Tensor x_in) {
+    torch::Tensor x = torch::clone(x_in);  // NOT IN PLACE! If we skip this line, then autograd gets messed up. FIXME.
     int n = x.size(1);
     for (int batch_item_index = 0; batch_item_index < x.size(0); ++batch_item_index) {
         int h = 1;
@@ -16,21 +17,22 @@ std::vector<torch::Tensor> fwht_forward(torch::Tensor x) {
             h *= 2;
         }
     }
-    return {x};
+    return x;
 }
 
-std::vector<torch::Tensor> fwht_backward(torch::Tensor grad_output) {
-    std::cout << "Got to the start" << std::endl;
-    int n_batches = grad_output.size(0);
-    std::cout << "Got to the start" << std::endl;
-    int n = grad_output.size(1);
-    std::cout << "Got to the start" << std::endl;
-    std::vector<torch::Tensor> result(n_batches);
-    for (int i = 0; i < n_batches; ++i) {
-        std::cout << i << std::endl;
-        result[i] = torch::eye(n);
-    }
-    return {result};
+at::Tensor fwht_forward(torch::Tensor x) {
+    return fwht(x);
+}
+
+torch::Tensor fwht_backward(torch::Tensor grad_output) {
+//    int n_batches = grad_output.size(0);
+//    int D = grad_output.size(1);
+//    torch::Tensor result = torch::zeros({n_batches, D, D});
+//    for (int i = 0; i < n_batches; ++i) {
+//        result[i] = torch::eye(D);
+//    }
+//    return {result};
+    return fwht(grad_output);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
