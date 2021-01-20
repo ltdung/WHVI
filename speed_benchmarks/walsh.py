a754cd3d5381a209195ada9b8679854c54a89457
David Nabergoj <davidnabergoj4@gmail.com>
2021-01-20 08:58:03 +0100

    Remove FWHT benchmark on diagonal matrices.

--- speed_benchmarks/walsh.py
+++ speed_benchmarks/walsh.py
@@ -1,6 +1,7 @@
 from speed_benchmarks.classes import Benchmark, st_time
 import torch
-from src.walsh import build_H, fwht, FWHT, FWHT_diag
+from src.utils import build_H
+from src.fwht.cpp.fwht import FWHTFunction
 
 
 class WalshBenchmark(Benchmark):
@@ -9,76 +10,36 @@ class WalshBenchmark(Benchmark):
         H @ A
 
     @st_time
-    def fwht_(self, A):
-        fwht(A)
-
-    @st_time
-    def FWHT_(self, A):
-        FWHT.apply(A)
-
-    @st_time
-    def FWHT_diag_(self, D):
-        FWHT_diag.apply(D)
+    def FWHTFunction_(self, A):
+        FWHTFunction.apply(A)
 
     def benchmark_FWHT_speed_small(self):
         D = 2 ** 2
         A = torch.randn(D, D)
-        H = build_H(D)
+        H = build_H(D, device=torch.device("cpu"))
         self.matmul(H, A)
-        self.fwht_(A)
-        self.FWHT_(A)
+        self.FWHTFunction_(A.unsqueeze(0))
 
     def benchmark_FWHT_speed_med(self):
         D = 2 ** 7
         A = torch.randn(D, D)
-        H = build_H(D)
+        H = build_H(D, device=torch.device("cpu"))
         self.matmul(H, A)
-        self.fwht_(A)
-        self.FWHT_(A)
+        self.FWHTFunction_(A.unsqueeze(0))
 
     def benchmark_FWHT_speed_large(self):
         D = 2 ** 12
         A = torch.randn(D, D)
-        H = build_H(D)
+        H = build_H(D, device=torch.device("cpu"))
         self.matmul(H, A)
-        self.fwht_(A)
-        self.FWHT_(A)
+        self.FWHTFunction_(A.unsqueeze(0))
 
     def benchmark_FWHT_speed_very_large(self):
         D = 2 ** 13
         A = torch.randn(D, D)
-        H = build_H(D)
+        H = build_H(D, device=torch.device("cpu"))
         self.matmul(H, A)
-        self.fwht_(A)
-        self.FWHT_(A)
-
-    def benchmark_FWHT_diag_speed_small(self):
-        D = 2 ** 2
-        elements = torch.randn(D)
-        H = build_H(D)
-        self.matmul(H, torch.diag(elements))
-        self.FWHT_diag_(elements)
-
-    def benchmark_FWHT_diag_speed_med(self):
-        D = 2 ** 7
-        elements = torch.randn(D)
-        H = build_H(D)
-        self.matmul(H, torch.diag(elements))
-        self.FWHT_diag_(elements)
-
-    def benchmark_FWHT_diag_speed_large(self):
-        D = 2 ** 12
-        elements = torch.randn(D)
-        H = build_H(D)
-        self.matmul(H, torch.diag(elements))
-        self.FWHT_diag_(elements)
-
-    def benchmark_FWHT_diag_speed_very_large(self):
-        D = 2 ** 13
-        elements = torch.randn(D)
-        H = build_H(D)
-        self.matmul(H, torch.diag(elements))
-        self.FWHT_diag_(elements)
+        self.FWHTFunction_(A.unsqueeze(0))
 
 
 if __name__ == '__main__':
